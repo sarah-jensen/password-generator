b87d09fe0457a1f95a5197d0021b82bacecad490
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-02-26 10:14:27 -0800

    debugging for loop with BCS

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function writePassword() {
 
   // Capture user input
   // determine how many characters long the password should be
-  const howLong = prompt('How long do you want your password to be? Enter a number from 8-128.');
+  var howLong = prompt('How long do you want your password to be? Enter a number from 8-128.');
   // alert for too short or too long
   if (howLong <= 7 || howLong > 128) {
     alert('You must enter a valid number.');
@@ -32,9 +32,9 @@ function writePassword() {
     alert('You must enter a valid number.');
     return;
   } else {
-    var pwdchar = howLong;
-    parseInt(howLong);
-    console.log(howLong);
+    // parseInt(howLong);
+    var pwdchar = parseInt(howLong);
+    console.log(pwdchar);
   }
 
   // confirm types of character sets to be included
@@ -119,18 +119,21 @@ function writePassword() {
   }
 
 
-// randomly select the correct number of characters based on user response to howLong prompt
-const randomize =  function() {
+  // randomly select the correct number of characters based on user response to howLong prompt
+  const randomize = function () {
 
-  let pwdArray = [''];
-  var char = requiredChar[Math.floor(Math.random() * pwdchar.length)];
-  // uses loop to push characters to password array
-  for (i = 0; i < pwdchar.length; i++); {
-    pwdArray.push(char);
+    // uses loop to push characters to password array
+    for (i = 0; i < pwdchar; i++); {
+      var pwdArray = [];
+      var char = requiredChar[Math.floor(Math.random() * pwdchar)];
+      pwdArray.push(char);
+      console.log(char);
+
+    }
     console.log(pwdArray);
   }
 
-}
+  randomize();
 
 }
 
@@ -155,18 +158,9 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+// left off trying to get pwdArray to include the right number of characters
 
-// // GIVEN I need a new, secure password
-// // WHEN I click the button to generate a password
-// // THEN I am presented with a series of prompts for password criteria
-// // WHEN prompted for password criteria
-// // THEN I select which criteria to include in the password
-// // WHEN prompted for the length of the password
-// // THEN I choose a length of at least 8 characters and no more than 128 characters
-// // WHEN asked for character types to include in the password
-// // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// // WHEN I answer each prompt
-// // THEN my input should be validated and at least one character type should be selected
+
 // // WHEN all prompts are answered
 // // THEN a password is generated that matches the selected criteria
 // // WHEN the password is generated
